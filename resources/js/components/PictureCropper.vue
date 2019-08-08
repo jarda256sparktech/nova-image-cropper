7e951cb55e3547e5c5ee49f701fb68709fe3f60d
Jaroslav Stoklasa <stoklasa.jaroslav@icloud.com>
2019-08-08 21:02:25 +0200

    modify

--- resources/js/components/PictureCropper.vue
+++ resources/js/components/PictureCropper.vue
@@ -1,120 +1,134 @@
 <template>
-  <div
-    ref="container"
-    class="container"
-  >
-    <img
-      ref="img"
-      :src="image"
-      alt="Picture"
-    >
-  </div>
+	<div
+			ref="container"
+			class="container"
+	>
+		<img
+				ref="img"
+				:src="image"
+				alt="Picture"
+		>
+	</div>
 </template>
 <script>
-import Cropper from 'cropperjs'
-import 'cropperjs/dist/cropper.css'
+	import Cropper from 'cropperjs'
+	import 'cropperjs/dist/cropper.css'
 
-export default {
-  name: 'PictureCropper',
+	export default {
+		name: 'PictureCropper',
 
-  props: {
-    image: {
-      type: String,
-      default: ''
-    },
+		props: {
+			image: {
+				type: String,
+				default: ''
+			},
 
-    ratio: {
-      type: Number,
-      default: 1
-    }
-  },
+			ratio: {
+				type: Number,
+				default: 1
+			}
+		},
 
-  data() {
-    return {
-      cropper: null,
-      width: 0,
-      height: 0
-    }
-  },
+		data() {
+			return {
+				cropper: null,
+				width: 0,
+				height: 0
+			}
+		},
 
-  watch: {
-    image(image) {
-      if (image) {
-        this.buildCropper()
-      }
-    }
-  },
+		watch: {
+			image(image) {
+				if (image) {
+					this.buildCropper()
+				}
+			}
+		},
 
-  mounted() {
-    window.addEventListener('resize', this.setWidth)
-    this.buildCropper()
-  },
+		mounted() {
+			window.addEventListener('resize', this.setWidth)
+			this.buildCropper()
+		},
 
-  destroyed() {
-    window.removeEventListener('resize', this.setWidth)
-    this.cropper.destroy()
-  },
+		destroyed() {
+			window.removeEventListener('resize', this.setWidth)
+			this.cropper.destroy()
+		},
 
-  methods: {
-    buildCropper() {
-      if (this.cropper) {
-        this.cropper.destroy()
-      }
+		methods: {
+			buildCropper() {
+				if (this.cropper) {
+					this.cropper.destroy()
+				}
 
-      this.setWidth()
-      const self = this
+				this.setWidth()
+				const self = this
 
-      this.$refs.img.addEventListener('cropmove', (event) => {
-        console.log(event.detail);
-        console.log(event.detail.originalEvent);
-        console.log(event.detail.action);
-      });
-      this.cropper = new Cropper(this.$refs.img, {
-        viewMode: 1,
-        dragMode: 'none',
-        autoCropArea: 1,
-        aspectRatio: self.ratio,
-        checkCrossOrigin: false,
-        movable: false,
-        rotatable: false,
-        zoomable: false,
-        minContainerWidth: self.width,
-        minContainerHeight: self.height,
-        preview: '#cropped-preview'
-      })
-      this.cropper.replace(this.image)
-    },
+				this.$refs.img.addEventListener('cropmove', (event) => {
+					this.updateThumb()
+				});
+				this.$refs.img.addEventListener('cropstart', (event) => {
+					this.updateThumb()
+				});
 
-    setWidth() {
-      const width = this.$refs.container.clientWidth
-      if (!width) {
-        return
-      }
-      this.width = width
-      this.height = this.ratio ? this.width / this.ratio : this.height
-    },
+				this.cropper = new Cropper(this.$refs.img, {
+					viewMode: 1,
+					dragMode: 'none',
+					autoCropArea: 1,
+					aspectRatio: self.ratio,
+					checkCrossOrigin: false,
+					movable: false,
+					rotatable: false,
+					zoomable: false,
+					minContainerWidth: self.width,
+					minContainerHeight: self.height,
+					preview: '#cropped-preview'
+				})
+				this.cropper.replace(this.image)
+			},
 
-    getCroppedCanvas() {
-      if (!this.cropper) {
-        return
-      }
-      return this.cropper.getCroppedCanvas()
-    },
+			updateThumb() {
+				const canvas = this.getCroppedCanvas();
+				if (canvas) {
+				    const thumbFile = canvas.toDataURL();
+				    console.log('thumbFile',thumbFile);
+					this.$emit('thumbFileChanged', thumbFile)
+				}
+			},
 
-    replace(image) {
-      this.image = image
 
-      this.buildCropper()
-    }
-  }
-}
+			setWidth() {
+				const width = this.$refs.container.clientWidth
+				if (!width) {
+					return
+				}
+				this.width = width
+				this.height = this.ratio ? this.width / this.ratio : this.height
+			},
+
+			getCroppedCanvas() {
+				if (!this.cropper) {
+					return
+				}
+				return this.cropper.getCroppedCanvas()
+			},
+
+			replace(image) {
+				this.image = image
+
+				this.buildCropper()
+			}
+		}
+	}
 </script>
 <style scoped>
-.container {
-  max-width: 640px;
-  margin: 20px auto;
-}
-img {
-  width: 100%;
-  max-width: 100%;
-}
+	.container {
+		max-width: 640px;
+		margin: 20px auto;
+	}
+
+	img {
+		width: 100%;
+		max-width: 100%;
+	}
+</style>
